465959c1b4b617f9d9815bd2d13936d205975768
Padmapriya S <158708326+PRIYAtechky@users.noreply.github.com>
2024-12-10 11:49:20 +0530

    Update Day 1 - Second Largest.py

--- Arrays/Day 1 - Second Largest.py
+++ Arrays/Day 1 - Second Largest.py
@@ -1,3 +1,16 @@
+"""
+Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+Note: The second largest element should not be equal to the largest element.
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+Input: arr[] = [10, 5, 10]
+Output: 5
+Explanation: The largest element of the array is 10 and the second largest element is 5. 
+"""
+
 class Solution:
     def getSecondLargest(self, arr):
         if not arr or len(arr) < 2:
