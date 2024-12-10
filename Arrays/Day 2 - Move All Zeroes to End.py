0672f1a3a0080fa2f346e6e4020ed6a3294ee715
Padmapriya S <158708326+PRIYAtechky@users.noreply.github.com>
2024-12-10 11:53:03 +0530

    Update Day 2 - Move All Zeroes to End.py

--- Arrays/Day 2 - Move All Zeroes to End.py
+++ Arrays/Day 2 - Move All Zeroes to End.py
@@ -10,6 +10,10 @@ Explanation: There are three 0s that are moved to the end.
 Input: arr[] = [10, 20, 30]
 Output: [10, 20, 30]
 Explanation: No change in array as there are no 0s.
+
+Input: arr[] = [0, 0]
+Output: [0, 0]
+Explanation: No change in array as there are all 0s.
 """
 
 class Solution:
